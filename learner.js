f3af84f7729e4b2e901bd52d2756a5715c49a5aa
Jonathan Holtmann <holtmann@usc.edu>
2019-04-13 18:59:41 -0700

    Basic electron framework

---
+++ learner.js
@@ -0,0 +1,128 @@
+// Imports modules.
+const fs = require(`fs`);
+const path = require(`path`);
+
+const transcriber = require('./transcript.js')
+const voice = require('./voiceResponse.js')
+const translate = require('./translate.js')
+
+const AudioRecorder = require(`node-audiorecorder`);
+
+// Constants.
+const DIRECTORY = `examples-recordings`;
+
+// Initialize recorder and file stream.
+const audioRecorder = new AudioRecorder({
+	program: process.platform === `win32` ? `sox` : `rec`,
+  // Following options only available when using `rec` or `sox`.
+  silence: 3,         // Duration of silence in seconds before it stops recording.
+  thresholdStart: 0.5,  // Silence threshold to start recording.
+  thresholdStop: 0.5,   // Silence threshold to stop recording.
+}, console);
+
+// Create path to write recordings to.
+if (!fs.existsSync(DIRECTORY)){
+	fs.mkdirSync(DIRECTORY);
+}
+
+// Create file path with random name.
+const fileName = path.join(DIRECTORY, "audio.wav");
+
+console.log(`Writing new recording file at: `, fileName);
+
+const axios = require('axios')
+
+function getResponse(context) {
+	var cont
+
+	if (context.size > 3) {
+		for (var i = context.size - 4; i < context.size; i++) {
+			cont.push(context[i])
+		}
+	} else {
+		cont = context
+	}
+
+	console.log(cont)
+
+	return new Promise((resolve, reject) => {
+		axios.post('http://localhost:8080/cakechat_api/v1/actions/get_response', {
+			context : cont,
+			emotion : 'neutral'
+		}).then((res) => {
+			console.log("Message from chat bot: " + res.data.response)
+			resolve(res.data.response)
+		}).catch((error) => {
+			reject(error)
+		})
+	})
+}
+
+var sourceLang = 'de';
+var destLang = 'en';
+var responseLanguage = 'en';
+
+var conversation = new Array()
+
+function askRespond () {
+	// Create write stream.
+	const fileStream = fs.createWriteStream(fileName, { encoding: `binary` });
+
+	// Start and write to the file.
+	audioRecorder.start().stream().pipe(fileStream);
+
+	// Log information on the following events
+	audioRecorder.stream().on(`close`, function(code) {
+		console.warn(`Recording closed. Exit code: `, code);
+	});
+
+	audioRecorder.stream().on(`end`, function() {
+		console.warn(`Recording ended.`);
+	});
+
+	audioRecorder.stream().on(`error`, function() {
+		console.warn(`Recording error.`);
+	});
+
+	setTimeout(() => {
+		audioRecorder.stop()
+		//make sure to set language accordingly
+		var prom = transcriber.syncRecognizeWords("examples-recordings/audio.wav", "LINEAR16", 16000, sourceLang);
+
+		//get transcribe, put through translateText(transcriber output, desired language to convert)
+
+		prom.then((arg) => {
+			console.log("Transcribed: " + arg);
+
+			if (!arg) {
+				askRespond()
+				return;
+			}
+
+			var translatePromise = translate.translateText(arg, destLang)
+			translatePromise.then((translatedText) => {
+				console.log("Translated to: " + translatedText);
+
+				conversation.push(translatedText)
+
+				var chatbotResponsePromise = getResponse(conversation);
+				chatbotResponsePromise.then((chatbotResponse) => {
+
+					var translateBackPromise = translate.translateText(chatbotResponse, responseLanguage)
+					translateBackPromise.then((text) => {
+						console.log("Translated back to: " + text)
+						var voiceProm = voice.voiceRespond(text, responseLanguage);
+						voiceProm.then((arg) => {
+							var doneSpeakingPromise = voice.speak(arg);
+							doneSpeakingPromise.then(() => {
+								askRespond()
+							})
+						})
+					})
+				})
+			})
+		})
+	}, 3000)
+}
+
+module.exports = Object.assign({askRespond})
--- main.js
+++ main.js
@@ -1,128 +1,71 @@
-// Imports modules.
-const fs = require(`fs`);
-const path = require(`path`);
+const electron = require('electron')
 
-const transcriber = require('./transcript.js')
-const voice = require('./voiceResponse.js')
-const translate = require('./translate.js')
 
-const AudioRecorder = require(`node-audiorecorder`);
+const { ipcMain } = require('electron')
 
-// Constants.
-const DIRECTORY = `examples-recordings`;
+// Module to control application life.
+const app = electron.app
 
-// Initialize recorder and file stream.
-const audioRecorder = new AudioRecorder({
-	program: process.platform === `win32` ? `sox` : `rec`,
-  // Following options only available when using `rec` or `sox`.
-  silence: 3,         // Duration of silence in seconds before it stops recording.
-  thresholdStart: 0.5,  // Silence threshold to start recording.
-  thresholdStop: 0.5,   // Silence threshold to stop recording.
-}, console);
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
-// Create path to write recordings to.
-if (!fs.existsSync(DIRECTORY)){
-	fs.mkdirSync(DIRECTORY);
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 600, height: 800, webPreferences: {nodeIntegration: true}, show: false})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
 }
 
-// Create file path with random name.
-const fileName = path.join(DIRECTORY, "audio.wav");
+// SETUP
+app.setAppUserModelId(process.execPath)
 
-console.log(`Writing new recording file at: `, fileName);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
-const axios = require('axios')
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-function getResponse(context) {
-	var cont
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
 
-	if (context.size > 3) {
-		for (var i = context.size - 4; i < context.size; i++) {
-			cont.push(context[i])
-		}
-	} else {
-		cont = context
-	}
+// In main process.
+app.on('ready', function () {
 
-	console.log(cont)
+	ipcMain.on('start', (event, arg) => {
 
-	return new Promise((resolve, reject) => {
-		axios.post('http://localhost:8080/cakechat_api/v1/actions/get_response', {
-			context : cont,
-			emotion : 'neutral'
-		}).then((res) => {
-			console.log("Message from chat bot: " + res.data.response)
-			resolve(res.data.response)
-		}).catch((error) => {
-			reject(error)
-		})
-	})
-}
+  })
 
-var sourceLang = 'de';
-var destLang = 'en';
-var responseLanguage = 'en';
-
-var conversation = new Array()
-
-askRespond()
-
-function askRespond () {
-	// Create write stream.
-	const fileStream = fs.createWriteStream(fileName, { encoding: `binary` });
-
-	// Start and write to the file.
-	audioRecorder.start().stream().pipe(fileStream);
-
-	// Log information on the following events
-	audioRecorder.stream().on(`close`, function(code) {
-		console.warn(`Recording closed. Exit code: `, code);
-	});
-
-	audioRecorder.stream().on(`end`, function() {
-		console.warn(`Recording ended.`);
-	});
-
-	audioRecorder.stream().on(`error`, function() {
-		console.warn(`Recording error.`);
-	});
-
-	setTimeout(() => {
-		audioRecorder.stop()
-		//make sure to set language accordingly
-		var prom = transcriber.syncRecognizeWords("examples-recordings/audio.wav", "LINEAR16", 16000, sourceLang);
-
-		//get transcribe, put through translateText(transcriber output, desired language to convert)
-
-		prom.then((arg) => {
-			console.log("Transcribed: " + arg);
-
-			if (!arg) {
-				askRespond()
-				return;
-			}
-
-			var translatePromise = translate.translateText(arg, destLang)
-			translatePromise.then((translatedText) => {
-				console.log("Translated to: " + translatedText);
-
-				conversation.push(translatedText)
-
-				var chatbotResponsePromise = getResponse(conversation);
-				chatbotResponsePromise.then((chatbotResponse) => {
-
-					var translateBackPromise = translate.translateText(chatbotResponse, responseLanguage)
-					translateBackPromise.then((text) => {
-						console.log("Translated back to: " + text)
-						var voiceProm = voice.voiceRespond(text, responseLanguage);
-						voiceProm.then((arg) => {
-							var doneSpeakingPromise = voice.speak(arg);
-							doneSpeakingPromise.then(() => {
-								askRespond()
-							})
-						})
-					})
-				})
-			})
-		})
-	}, 3000)
-}
+})
