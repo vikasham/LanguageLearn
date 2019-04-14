5516ef01c76dfc34372804d5fe60cbc5a4c5ecca
Vikash Mody <vikasham@usc.edu>
2019-04-13 18:31:53 -0700

    POST requests to server and loop for conversation

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const DIRECTORY = `examples-recordings`;
 const audioRecorder = new AudioRecorder({
 	program: process.platform === `win32` ? `sox` : `rec`,
   // Following options only available when using `rec` or `sox`.
-  silence: 1,         // Duration of silence in seconds before it stops recording.
+  silence: 3,         // Duration of silence in seconds before it stops recording.
   thresholdStart: 0.5,  // Silence threshold to start recording.
   thresholdStop: 0.5,   // Silence threshold to stop recording.
 }, console);
@@ -30,84 +30,99 @@ const fileName = path.join(DIRECTORY, "audio.wav");
 
 console.log(`Writing new recording file at: `, fileName);
 
-// Create write stream.
-const fileStream = fs.createWriteStream(fileName, { encoding: `binary` });
-
-// Start and write to the file.
-audioRecorder.start().stream().pipe(fileStream);
-
-// Log information on the following events
-audioRecorder.stream().on(`close`, function(code) {
-	console.warn(`Recording closed. Exit code: `, code);
-});
-
-audioRecorder.stream().on(`end`, function() {
-	console.warn(`Recording ended.`);
-});
-
-audioRecorder.stream().on(`error`, function() {
-	console.warn(`Recording error.`);
-});
-
-const request = require('request')
+const axios = require('axios')
 
 function getResponse(context) {
 	var cont
-	for (var i = context.size() - 4; i < context.size(); i++) {
-		cont.push(context[i])
+
+	if (context.size > 3) {
+		for (var i = context.size - 4; i < context.size; i++) {
+			cont.push(context[i])
+		}
+	} else {
+		cont = context
 	}
 
+	console.log(cont)
+
 	return new Promise((resolve, reject) => {
-		request.post({
-			url : 'http://localhost:8080/cakechat_api/v1/actions/get_response',
-			form : {
-				context : cont,
-				emotion : 'neutral'
-			},
-			json : true
-		}, (err, httpResponse, body) => {
-			if (err) reject(err)
-			
-			console.log("Received: " + httpResponse.response)
-			resolve(httpResponse.response)
-		}
+		axios.post('http://localhost:8080/cakechat_api/v1/actions/get_response', {
+			context : cont,
+			emotion : 'neutral'
+		}).then((res) => {
+			console.log("Message from chat bot: " + res.data.response)
+			resolve(res.data.response)
+		}).catch((error) => {
+			reject(error)
+		})
 	})
 }
 
-
-var sourceLang = 'ko';
+var sourceLang = 'de';
 var destLang = 'en';
+var responseLanguage = 'en';
 
 var conversation = new Array()
 
-setTimeout(() => {
-	audioRecorder.stop()
-	//make sure to set language accordingly
-	var prom = transcriber.syncRecognizeWords("examples-recordings/audio.wav", "LINEAR16", 16000, sourceLang);
+askRespond()
 
-	//get transcribe, put through translateText(transcriber output, desired language to convert)
+function askRespond () {
+	// Create write stream.
+	const fileStream = fs.createWriteStream(fileName, { encoding: `binary` });
 
-	prom.then((arg) => {
-		console.log("Transcribed: " + arg);
+	// Start and write to the file.
+	audioRecorder.start().stream().pipe(fileStream);
 
-		var translatePromise = translate.translateText(arg, destLang)
-		translatePromise.then((translatedText) => {
-			console.log("Translated to: " + translatedText);
+	// Log information on the following events
+	audioRecorder.stream().on(`close`, function(code) {
+		console.warn(`Recording closed. Exit code: `, code);
+	});
 
-			conversation.push(translatedText)
+	audioRecorder.stream().on(`end`, function() {
+		console.warn(`Recording ended.`);
+	});
 
-			var chatbotResponsePromise = getResponse(conversation);
-			chatbotResponsePromise.then((response) => {
-				var responseLanguage = sourceLang;
-				var translateBackPromise = translate.translateText(chatbotResponse, responseLanguage)
-				translateBackPromise.then((text) => {
-					console.log("Translated back to: " + text)
-					var voiceProm = voice.voiceRespond(text, responseLanguage);
-					voiceProm.then((arg) => {
-						voice.speak(arg);
+	audioRecorder.stream().on(`error`, function() {
+		console.warn(`Recording error.`);
+	});
+
+	setTimeout(() => {
+		audioRecorder.stop()
+		//make sure to set language accordingly
+		var prom = transcriber.syncRecognizeWords("examples-recordings/audio.wav", "LINEAR16", 16000, sourceLang);
+
+		//get transcribe, put through translateText(transcriber output, desired language to convert)
+
+		prom.then((arg) => {
+			console.log("Transcribed: " + arg);
+
+			if (!arg) {
+				askRespond()
+				return;
+			}
+
+			var translatePromise = translate.translateText(arg, destLang)
+			translatePromise.then((translatedText) => {
+				console.log("Translated to: " + translatedText);
+
+				conversation.push(translatedText)
+
+				var chatbotResponsePromise = getResponse(conversation);
+				chatbotResponsePromise.then((chatbotResponse) => {
+
+					var translateBackPromise = translate.translateText(chatbotResponse, responseLanguage)
+					translateBackPromise.then((text) => {
+						console.log("Translated back to: " + text)
+						var voiceProm = voice.voiceRespond(text, responseLanguage);
+						voiceProm.then((arg) => {
+							var doneSpeakingPromise = voice.speak(arg);
+							doneSpeakingPromise.then(() => {
+								askRespond()
+							})
+						})
 					})
 				})
 			})
 		})
-	})
-}, 3000)
+	}, 3000)
+}
---
+++ src/components/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+import '../css/App.css'
+import logo from '../img/logo.svg'
+import Button from './Button'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    // state is initialized in JSON notation
+    this.state = {
+      name: "User",
+      age: 21,
+      status: ""
+    }
+
+    // you must add this line!
+    this.handleChange = this.handleChange.bind(this)
+    // global methods for a component are not bound to the current instance
+    // automatically this is just how javascript works
+
+  }
+
+  // a pre-existing lifecycle method that we are overriding
+  // this will run right after the component is created in the DOM
+  // if you add and remove components, it will run every single time
+  // the particular compnent was added
+  componentDidMount() {
+    // const user_name = prompt("What is your name?")
+    // prompt() allows you to prompt a user for an input
+
+    // this.state can only be accessed as a public member variable in the constructor
+    // it becomes private with get & set methods in other functions
+    // setState() takes in 1 argument, the object, representing the new state items
+    this.setState({
+      status: "Current status"
+    })
+  }
+
+  // event handlers are passed in an event variable, we'll call ours "e"
+  handleChange(e) {
+    this.setState({
+      // for target <tag> of this element's event "e", select its "value" attribute
+      // assign the "status" state to this value
+      status: e.target.value
+    })
+  }
+
+  render() {
+    const { name, age, status} = this.state // save all the state variables
+    return (
+      <div className="App">
+      <Button/>
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {name}, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <input type="text" name="Status" placeholder=" good." value={status} onChange={this.handleChange}></input>
+          {/* This is a JSX Style Comment */}
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
